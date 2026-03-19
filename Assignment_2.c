e1dce428af0ab65f665dd22dbb0e271a52e97ac1
RishikaAgarwal-Tech <agarwalrishika305@gmail.com>
2026-03-19 07:14:01 +0000

    Fixed syntax error

---
+++ Assignment_2.c
@@ -0,0 +1,24 @@
+/*Write a C program to perform basic arithmetric operations*/
+# include <stdio.h>
+int main ()
+{
+    int a, b, c ;
+    printf ("Enter the first number: ") ;
+    scanf ("%d", &a) ;
+    printf ("Enter the second number: ") ;
+    scanf ("%d", &b) ;
+
+    c = a + b ;
+    printf ("The addition of the numbers is %d\n", c) ;
+
+    c = a - b ;
+    printf ("The subtraction of the two numbers is %d\n", c) ;
+
+    c = a * b ;
+    printf ("The multiplication of the two numbers is %d\n", c);
+
+    c = a / b ;
+    printf ("The division of the two numbers is %d\n", c);
+
+    return 0 ;
+}
